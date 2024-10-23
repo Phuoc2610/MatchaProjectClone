db2ac07d59827b8b6aaee88b2ae2852f80e1445e
Van Phuoc <phuoc0905886611@gmail.com>
2024-10-23 17:52:43 +0700

    code

---
+++ page/Slide.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.slider-wrapper', {
+
+  loop: true,
+  slidesPerView: 3.3,
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+          
